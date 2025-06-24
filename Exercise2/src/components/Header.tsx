25db7a81eb2cb7e6fd93208feaef78ab99ebebac
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-06-24 22:48:36 +0200

    Done with header component

--- Exercise2/src/components/Header.tsx
+++ Exercise2/src/components/Header.tsx
@@ -1,40 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
+import logo from "../images/logo.png";
+type State = {
+  isMenuOpen: boolean;
+};
 
-interface Props {}
+class Header extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      isMenuOpen: false,
+    };
+  }
 
-const Header: React.FC<Props> = () => {
-  return (
-    <div className="Header">
-      <div className="cont">
-        <a href="/" className="logo-wrap">
-          <img alt="Logo" />
-        </a>
-        <div className="button_container" id="toggle">
-          <span className="top"></span>
-          <span className="middle"></span>
-          <span className="bottom"></span>
+  handleToggle = () => {
+    this.setState({
+      isMenuOpen: !this.state.isMenuOpen,
+    });
+  };
+
+  render(): React.ReactNode {
+    const isMenuOpen = this.state.isMenuOpen;
+    return (
+      <div className="Header">
+        <div className="cont">
+          <a href="/" className="logo-wrap">
+            <img src={logo} alt="Logo" />
+          </a>
+          <div
+            className={`button_container ${isMenuOpen ? "active" : ""}`}
+            id="toggle"
+            onClick={this.handleToggle}
+          >
+            <span className="top"></span>
+            <span className="middle"></span>
+            <span className="bottom"></span>
+          </div>
+        </div>
+        <div
+          className={`overlay ${this.state.isMenuOpen ? "open" : ""}`}
+          id="overlay"
+        >
+          <nav className="overlay-menu">
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">Current Weather</a>
+              </li>
+              <li>
+                <a href="/">Weather Forecast</a>
+              </li>
+              <li>
+                <a href="/">Open Weather Map</a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
-      <div className="overlay" id="overlay">
-        <nav className="overlay-menu">
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/">Current Weather</a>
-            </li>
-            <li>
-              <a href="/">Weather Forecast</a>
-            </li>
-            <li>
-              <a href="/">Open Weather Map</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Header;
